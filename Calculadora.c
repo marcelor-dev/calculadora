dfc329843ada485293a9909dd37a32017197e3a7
marcelor-dev <marcelo.rainier@hotmail.com>
2025-11-05 15:35:32 -0300

    Add files via upload

---
+++ Calculadora.c
@@ -0,0 +1,29 @@
+/* Programa calculadora */
+/* calculadora.c */
+
+#include <stdio.h>
+#include <locale.h>
+
+/* funcao para cadastro de pedidos */
+
+
+int main (void){
+	
+		setlocale(LC_ALL, "Portuguese");
+	
+	/* Layout de tela */
+	printf("**** Calculadora *****\n\n");
+	
+	printf("Informe o código do produto: \n");
+	printf("Informe o nome do produto: \n");
+	printf("Informe o código do produto: \n");
+	printf("Informe o peso do produto: \n");
+	printf("Informe o preço do produto: \n");
+	printf("Informe o código do local de entrega do produto : \n");
+	printf("1 - Região Sul\n");
+	printf("2 - Região Sudeste\n");
+	printf("3 - Região Norte\n");
+	printf("3 - Região Nordeste\n");
+	
+	
+}
